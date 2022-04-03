b3b9b0007da3a6a7f0aa63921a3e2f45e83a59ea
vishwaspuriofficial@gmail.com <80814270+vishwaspuriofficial@users.noreply.github.com>
2022-04-03 14:43:46 -0400

    updated

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 window.addEventListener('load', (event) => {
     console.log('The page has fully loaded');
-    const params = new URLSearchParams(window.location.search)
-    if (params.has('test')) {
-        if (params.get('test')) {
-            // document.title = params.get('test');
-            // return params.get('test');
-            document.cookie = "username=Max Brown";
-            console.log("Cookie Created");
-    }
-  }
+    document.cookie = "username=John Doe";
+    console.log("Cookie Created");
+    // const params = new URLSearchParams(window.location.search)
+    // if (params.has('test')) {
+    //     if (params.get('test')) {
+    //         // document.title = params.get('test');
+    //         // return params.get('test');
+    //         document.cookie = "username=John Doe";
+    //         console.log("Cookie Created");
+    // }
+//   }
   });
